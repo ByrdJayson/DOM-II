83234620b94bcff39a6da1c6b6778faef6275d7a
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-10-26 14:09:47 -0400

    3 Events down

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
 // Your code goes here
- 
+
+
+const welcome = document.querySelector('.intro h2');
+
+welcome.addEventListener('mouseover', function(){
+  welcome.style.display = 'none';
+})
+
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', event => {
+  if (event.key === 'Delete'){
+    body.style.display = 'none';
+  }
+})
+
+body.addEventListener('wheel', function(e){
+  body.style.color = 'green';
+})
+
+const buttons = document.querySelectorAll('button');
+const buttonsArray = Array.from(buttons);
+
+buttonsArray.forEach((item) => {
+  item.addEventListener('click', function(e){
+    e.target.style.color = 'orange';
+  })
+});
